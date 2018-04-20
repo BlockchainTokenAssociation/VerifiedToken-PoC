49cc2f8d06a679b7b23f9ffbc1de5b7d87a352c8
AT <tikonoff@gmail.com>
2018-04-20 13:50:44 +1200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
 var path = require('path');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: './app/js/app.js',
+    entry: './demo/app.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'demo/assets'),
     },
     plugins: [
-        // Copy our app's index.html to the build folder.
-        new CopyWebpackPlugin([
-            { from: './app/index.html', to: 'index.html' }
-        ])
     ],
     node: {
         fs: "empty"
