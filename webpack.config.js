762934114b353eeda16f6b063226d1c01289126a
AT <tikonoff@gmail.com>
2018-04-24 18:33:48 +1200

    attempt to update demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: [
     './demo/app.js',
     'webpack-dev-server/client?http://localhost:8080'
