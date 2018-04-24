43d459594f8cb4341a4be0711990f141bf9a66c0
AT <tikonoff@gmail.com>
2018-04-24 18:14:36 +1200

    tabs 4 to tabs 2

--- test/VerifiedTokenController.js
+++ test/VerifiedTokenController.js
@@ -4,9 +4,9 @@ import { toUtf8 } from './helpers/asciiConverter';
 const BigNumber = web3.BigNumber;
 
 const should = require('chai')
-    .use(require('chai-as-promised'))
-    .use(require('chai-bignumber')(BigNumber))
-    .should();
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 const Token = artifacts.require('TokenMock');
 const Controller = artifacts.require('Controller');
@@ -15,106 +15,95 @@ const Registry = artifacts.require('Registry');
 
 contract('Controller.sol', function ([deployer, registered, stranger]) {
 
-    before(async function () {
-        this.registry = await Registry.new();
-        this.cntlr = await Controller.new([this.registry.address], 1);
-        this.token = await Token.new(this.cntlr.address);
+  before(async function () {
+    this.registry = await Registry.new();
+    this.cntlr = await Controller.new([this.registry.address], 1);
+    this.token = await Token.new(this.cntlr.address);
+  });
+
+  describe('updateRequiredConfirmations()', function () {
+    it('should update confirmations number and fire an event', async function () {
+      const {logs} = await this.cntlr.updateRequiredConfirmations(5).should.be.fulfilled;
+      const event = logs.find(e => e.event === 'RequiredConfirmationsUpdated');
+      should.exist(event);
+      event.args.confirmations.toNumber().should.equal(5);
+    });
+  });
+
+  describe('updateRequiredData()', function () {
+    it('should fail if number of keys not the same as the number of values', async function () {
+      await this.cntlr.updateRequiredData(["token address","allowed age group"],["18+"]).should.be.rejectedWith(EVMThrow);
     });
 
-    describe('updateRequiredConfirmations()', function () {
-        it('should update confirmations number and fire an event', async function () {
-            const {logs} = await this.cntlr.updateRequiredConfirmations(5).should.be.fulfilled;
-            const event = logs.find(e => e.event === 'RequiredConfirmationsUpdated');
-            should.exist(event);
-            event.args.confirmations.toNumber().should.equal(5);
-        });
+    it('should update key => value pairs and fire an event for each pair', async function () {
+      const {logs} = await this.cntlr.updateRequiredData(["id type","allowed age group"],["passport","18+"]).should.be.fulfilled;
+      const event = logs.filter(e => e.event === 'RequiredDataUpdated');
+      should.exist(event);
+      event.length.should.equal(2);
+      toUtf8(event[0].args.key).should.equal("id type");
+      toUtf8(event[0].args.value).should.equal("passport");
+      toUtf8(event[1].args.key).should.equal("allowed age group");
+      toUtf8(event[1].args.value).should.equal("18+");
+    });
+  });
+
+  describe('isVerified()', function () {
+    it('should return TRUE if required number of confirmations = 0', async function () {
+      await this.cntlr.updateRequiredConfirmations(0);
+      (await this.cntlr.isVerified(stranger)).should.be.true;
     });
 
-    describe('updateRequiredData()', function () {
-        it('should fail if number of keys not the same as the number of values', async function () {
-            await this.cntlr.updateRequiredData(["token address","allowed age group"],["18+"]).should.be.rejectedWith(EVMThrow);
-        });
+    it('should return FALSE if required number of confirmations > 0', async function () {
+      await this.cntlr.updateRequiredConfirmations(2);
+      (await this.cntlr.isVerified(stranger)).should.be.false;
+    });
+  });
 
-        it('should update key => value pairs and fire an event for each pair', async function () {
-            const {logs} = await this.cntlr.updateRequiredData(["id type","allowed age group"],["passport","18+"]).should.be.fulfilled;
-            const event = logs.filter(e => e.event === 'RequiredDataUpdated');
-
-            should.exist(event);
-            event.length.should.equal(2);
-
-            toUtf8(event[0].args.key).should.equal("id type");
-            toUtf8(event[0].args.value).should.equal("passport");
-
-            toUtf8(event[1].args.key).should.equal("allowed age group");
-            toUtf8(event[1].args.value).should.equal("18+");
-        });
+  describe('isContract()', function () {
+    it('should return FALSE if address is not a contract', async function () {
+      (await this.cntlr.isContract(stranger)).should.be.false;
     });
 
-    describe('isVerified()', function () {
-        it('should return TRUE if required number of confirmations = 0', async function () {
-            await this.cntlr.updateRequiredConfirmations(0);
-            (await this.cntlr.isVerified(stranger)).should.be.true;
-        });
+    it('should return TRUE if address is a contract', async function () {
+      (await this.cntlr.isContract(this.registry.address)).should.be.true;
+    });
+  });
 
-        it('should return FALSE if required number of confirmations > 0', async function () {
-            await this.cntlr.updateRequiredConfirmations(2);
-            (await this.cntlr.isVerified(stranger)).should.be.false;
-        });
+  describe('updateRegistries()', function () {
+    it('should fail if registry address is not a contract', async function () {
+      await this.cntlr.updateRegistries([0x1]).should.be.rejectedWith(EVMThrow);
     });
 
-    describe('isContract()', function () {
-
-        it('should return FALSE if address is not a contract', async function () {
-            (await this.cntlr.isContract(stranger)).should.be.false;
-        });
-
-        it('should return TRUE if address is a contract', async function () {
-            (await this.cntlr.isContract(this.registry.address)).should.be.true;
-        });
-
+    it('should fail if registry is not a registry contract', async function () {
+      let res = await this.cntlr.updateRegistries([this.token.address]).should.be.rejectedWith(EVMThrow);
     });
 
-    describe('updateRegistries()', function () {
-
-        it('should fail if registry address is not a contract', async function () {
-            await this.cntlr.updateRegistries([0x1]).should.be.rejectedWith(EVMThrow);
-        });
-
-        it('should fail if registry is not a registry contract', async function () {
-          let res = await this.cntlr.updateRegistries([this.token.address]).should.be.rejectedWith(EVMThrow);
-        });
-
-      it('should fail if zero address', async function () {
-            await this.cntlr.updateRegistries([0x0]).should.be.rejectedWith(EVMThrow);
-        });
-
-        it('otherwise, should update list of registries and fire an event', async function () {
-            const {logs} = await this.cntlr.updateRegistries([this.registry.address, this.registry.address]).should.be.fulfilled;
-            const event = logs.find(e => e.event === 'AcceptedRegistriesUpdated');
-            should.exist(event);
-            event.args.registries.length.should.equal(2);
-            event.args.registries[0].should.equal(this.registry.address);
-            event.args.registries[1].should.equal(this.registry.address);
-        });
-
+    it('should fail if zero address', async function () {
+      await this.cntlr.updateRegistries([0x0]).should.be.rejectedWith(EVMThrow);
     });
+
+    it('otherwise, should update list of registries and fire an event', async function () {
+      const {logs} = await this.cntlr.updateRegistries([this.registry.address, this.registry.address]).should.be.fulfilled;
+      const event = logs.find(e => e.event === 'AcceptedRegistriesUpdated');
+      should.exist(event);
+      event.args.registries.length.should.equal(2);
+      event.args.registries[0].should.equal(this.registry.address);
+      event.args.registries[1].should.equal(this.registry.address);
+    });
+  });
 
   describe('getNumberOfConfirmationsRequired()', function () {
-
     it('should return the number', async function () {
       (await this.cntlr.getNumberOfConfirmationsRequired()).toNumber().should.equal(2);
       await this.cntlr.updateRequiredConfirmations(1);
       (await this.cntlr.getNumberOfConfirmationsRequired()).toNumber().should.equal(1);
     });
-
   });
 
   describe('getRequiredData()', function () {
-
     it('should return the data (key=>value pairs)', async function () {
       let result = await this.cntlr.getRequiredData();
       let pairs = [];
-
       for (let i = 0; i < result[0].length; i++) {
         let pair = {
           key:  result[0][i],
@@ -122,25 +111,20 @@ contract('Controller.sol', function ([deployer, registered, stranger]) {
         }
         pairs.push(pair)
       }
-
       expect(toUtf8(pairs[0].key)).to.be.equal("id type");
       expect(toUtf8(pairs[0].value)).to.be.equal("passport");
       expect(toUtf8(pairs[1].key)).to.be.equal("allowed age group");
       expect(toUtf8(pairs[1].value)).to.be.equal("18+");
-
     });
-
   });
 
   describe('getRegistries()', function () {
-
     it('should return the list of registries', async function () {
       let result = await this.cntlr.getRegistries();
       result.length.should.equal(2);
       result[0].should.equal(this.registry.address);
       result[1].should.equal(this.registry.address);
     });
-
   });
 
 });
--- test/VerifiedTokenRegistry.js
+++ test/VerifiedTokenRegistry.js
@@ -3,122 +3,107 @@ import { toUtf8 } from './helpers/asciiConverter';
 const BigNumber = web3.BigNumber;
 
 const should = require('chai')
-    .use(require('chai-as-promised'))
-    .use(require('chai-bignumber')(BigNumber))
-    .should();
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 const Registry = artifacts.require('RegistryMock');
 
 
 contract('Registry.sol', function ([deployer, registry, stranger, lawful, dartVeider]) {
 
-    before(async function () {
-        this.registry = await Registry.new();
+  before(async function () {
+    this.registry = await Registry.new();
+  });
+
+  describe('isKeyExist()', function () {
+    it('should return TRUE if key is exist', async function () {
+      (await this.registry.isKeyExist("some key")).should.be.false;
+    });
+  });
+
+  describe('addNewKey()', function () {
+    it('should return true on adding new key', async function () {
+       (await this.registry.isKeyExist("new key")).should.be.false;
+       await this.registry.testAddNewKey("new key").should.be.fulfilled;
+       (await this.registry.isKeyExist("new key")).should.be.true;
+    });
+  });
+
+  describe('updateAddress()', function () {
+    it('should add a new key if it does not exist yet', async function () {
+      (await this.registry.isKeyExist("age group")).should.be.false;
+      await this.registry.updateAddress(lawful, "age group", "10+").should.be.fulfilled;
+      (await this.registry.isKeyExist("age group")).should.be.true;
     });
 
-    describe('isKeyExist()', function () {
-
-        it('should return TRUE if key is exist', async function () {
-            (await this.registry.isKeyExist("some key")).should.be.false;
-        });
-
+    it('should update record', async function () {
+      await this.registry.updateAddress(lawful, "age group", "18+").should.be.fulfilled;
+      (await this.registry.verifyAddress(lawful, "age group", "18+")).should.be.true;
     });
 
-    describe('addNewKey()', function () {
+    it('should fire an event', async function () {
+      const {logs} = await this.registry.updateAddress(lawful, "id type", "passport").should.be.fulfilled;
+      const event = logs.find(e => e.event === 'AddressUpdated');
+      should.exist(event);
+      let registryValue = web3.fromAscii(toUtf8(event.args.registry.replace(/\u0000/g, '')));
+      let receiverValue = web3.fromAscii(toUtf8(event.args.receiver.replace(/\u0000/g, '')));
+      registryValue.should.equal(this.registry.address.toString());
+      receiverValue.should.equal(lawful.toString());
+      toUtf8(event.args.key).should.equal("id type");
+      toUtf8(event.args.value).should.equal("passport");
+    });
+  });
 
-        it('should return true on adding new key', async function () {
-            (await this.registry.isKeyExist("new key")).should.be.false;
-            await this.registry.testAddNewKey("new key").should.be.fulfilled;
-            (await this.registry.isKeyExist("new key")).should.be.true;
-        });
-
+  describe('verifyAddress()', function () {
+    it('should return TRUE if address was found', async function () {
+      (await this.registry.verifyAddress(lawful, "age group", "18+")).should.be.true;
     });
 
-    describe('updateAddress()', function () {
-
-        it('should add a new key if it does not exist yet', async function () {
-            (await this.registry.isKeyExist("age group")).should.be.false;
-            await this.registry.updateAddress(lawful, "age group", "10+").should.be.fulfilled;
-            (await this.registry.isKeyExist("age group")).should.be.true;
-        });
-
-        it('should update record', async function () {
-            await this.registry.updateAddress(lawful, "age group", "18+").should.be.fulfilled;
-            (await this.registry.verifyAddress(lawful, "age group", "18+")).should.be.true;
-        });
-
-        it('should fire an event', async function () {
-            const {logs} = await this.registry.updateAddress(lawful, "id type", "passport").should.be.fulfilled;
-            const event = logs.find(e => e.event === 'AddressUpdated');
-            should.exist(event);
-            let registryValue = web3.fromAscii(toUtf8(event.args.registry.replace(/\u0000/g, '')));
-            let receiverValue = web3.fromAscii(toUtf8(event.args.receiver.replace(/\u0000/g, '')));
-            registryValue.should.equal(this.registry.address.toString());
-            receiverValue.should.equal(lawful.toString());
-            toUtf8(event.args.key).should.equal("id type");
-            toUtf8(event.args.value).should.equal("passport");
-        });
-
+    it('should return FALSE if ADDRESS was not found', async function () {
+      (await this.registry.verifyAddress(stranger, "age group", "18+")).should.be.false;
     });
 
-    describe('verifyAddress()', function () {
+    it('should return FALSE if address was NOT found in the KEY => VALUE pair', async function () {
+      (await this.registry.verifyAddress(lawful, "age group", "30")).should.be.false;
+    });
+  });
 
-        it('should return TRUE if address was found', async function () {
-            (await this.registry.verifyAddress(lawful, "age group", "18+")).should.be.true;
-        });
-
-        it('should return FALSE if ADDRESS was not found', async function () {
-            (await this.registry.verifyAddress(stranger, "age group", "18+")).should.be.false;
-        });
-
-        it('should return FALSE if address was NOT found in the KEY => VALUE pair', async function () {
-            (await this.registry.verifyAddress(lawful, "age group", "30")).should.be.false;
-        });
-
+  describe('deleteAddress()', function () {
+    it('should fire an event', async function () {
+      const {logs} = await this.registry.deleteAddress(lawful).should.be.fulfilled;
+      const event = logs.find(e => e.event === 'AddressDeleted');
+      should.exist(event);
+      let registryValue = web3.fromAscii(toUtf8(event.args.registry.replace(/\u0000/g, '')));
+      let receiverValue = web3.fromAscii(toUtf8(event.args.receiver.replace(/\u0000/g, '')));
+      registryValue.should.equal(this.registry.address.toString());
+      receiverValue.should.equal(lawful.toString());
     });
 
-    describe('deleteAddress()', function () {
-
-        it('should fire an event', async function () {
-            const {logs} = await this.registry.deleteAddress(lawful).should.be.fulfilled;
-            const event = logs.find(e => e.event === 'AddressDeleted');
-            should.exist(event);
-            let registryValue = web3.fromAscii(toUtf8(event.args.registry.replace(/\u0000/g, '')));
-            let receiverValue = web3.fromAscii(toUtf8(event.args.receiver.replace(/\u0000/g, '')));
-            registryValue.should.equal(this.registry.address.toString());
-            receiverValue.should.equal(lawful.toString());
-        });
-
-        it('record should be deleted', async function () {
-            (await this.registry.verifyAddress(lawful, "id type", "passport")).should.be.false;
-        });
-
-
+    it('record should be deleted', async function () {
+      (await this.registry.verifyAddress(lawful, "id type", "passport")).should.be.false;
     });
+  });
 
-    describe('exposeAddress()', function () {
-
-      it('should return key=>value pairs for given address', async function () {
-        await this.registry.updateAddress(dartVeider, "Side", "Dark").should.be.fulfilled;
-        await this.registry.updateAddress(dartVeider, "Has son", "yes").should.be.fulfilled;
-        let result = await this.registry.exposeAddress(dartVeider);
-        let pairs = [];
-
-        for (let i = 0; i < result[0].length; i++) {
-          let pair = {
-            key:  result[0][i],
-            value: result[1][i],
-          }
-          pairs.push(pair)
+  describe('exposeAddress()', function () {
+    it('should return key=>value pairs for given address', async function () {
+      await this.registry.updateAddress(dartVeider, "Side", "Dark").should.be.fulfilled;
+      await this.registry.updateAddress(dartVeider, "Has son", "yes").should.be.fulfilled;
+      let result = await this.registry.exposeAddress(dartVeider);
+      let pairs = [];
+      for (let i = 0; i < result[0].length; i++) {
+        let pair = {
+          key:  result[0][i],
+          value: result[1][i],
         }
-
-        expect(pairs.length).to.be.equal(2);
-        expect(toUtf8(pairs[0].key)).to.be.equal("Side");
-        expect(toUtf8(pairs[0].value)).to.be.equal("Dark");
-        expect(toUtf8(pairs[1].key)).to.be.equal("Has son");
-        expect(toUtf8(pairs[1].value)).to.be.equal("yes");
-      });
-
+        pairs.push(pair)
+      }
+      expect(pairs.length).to.be.equal(2);
+      expect(toUtf8(pairs[0].key)).to.be.equal("Side");
+      expect(toUtf8(pairs[0].value)).to.be.equal("Dark");
+      expect(toUtf8(pairs[1].key)).to.be.equal("Has son");
+      expect(toUtf8(pairs[1].value)).to.be.equal("yes");
     });
+  });
 
 });
