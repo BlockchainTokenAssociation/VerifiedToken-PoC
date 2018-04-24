bb67451dbb7ba5a4668dea0e63f5861fdbe05ac8
AT <tikonoff@gmail.com>
2018-04-24 18:37:07 +1200

    ws: -> wss:

--- demo/app.js
+++ demo/app.js
@@ -764,7 +764,7 @@ function init() {
     UIkit.notification('Web3 initialized.')
     localWeb3 = new Web3(window.web3.currentProvider)
   }
-  wsWeb3 = new Web3(new Web3.providers.WebsocketProvider('ws://kovan.infura.io/ws'));
+  wsWeb3 = new Web3(new Web3.providers.WebsocketProvider('wss://kovan.infura.io/ws'));
 
 }
 
